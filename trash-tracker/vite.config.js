7c4274a50968f38a8e9a49d6ba14905ff8c3dab8
Mauro <mauro.ongena@icloud.com>
2026-01-13 17:23:40 +0100

    removed github homepage setting

--- trash-tracker/vite.config.js
+++ trash-tracker/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/trash-tracker/",
-  "homepage": "https://mauroongena.github.io/trash-tracker/"
+  // base: "/trash-tracker/",
+  // "homepage": "https://mauroongena.github.io/trash-tracker/"
 })
