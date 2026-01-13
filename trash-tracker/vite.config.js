32d975366ac3d062aaa3a08f200e825583f92b03
Mauro <mauro.ongena@icloud.com>
2026-01-13 15:02:24 +0100

    Added github workflow

--- trash-tracker/vite.config.js
+++ trash-tracker/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/trash-tracker/",
+  "homepage": "https://mauroongena.github.io/trash-tracker/"
 })
